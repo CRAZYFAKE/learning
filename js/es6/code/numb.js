3df88232645b7ccac1a13bb15d4c264f0f90013c
姚义祥 <yaoyixiang@idoool.com>
2018-06-19 19:32:03 +0800

    update

--- js/es6/code/numb.js
+++ js/es6/code/numb.js
@@ -21,6 +21,25 @@ console.log(Number.parseFloat('123.45#')) // 123.45
 console.log(Number.EPSILON);
 console.log(Number.EPSILON.toFixed(20));
 
+/**
+ * 极小值可以用来设置 能够接受的误差范围
+ * 设置一个非常小的值, 如果两个数的差小于这个值, 那么可以认为这两个值相等
+ **/
+0.1 + 0.2
+// 0.30000000000000004
+
+0.1 + 0.2 - 0.3
+// 5.551115123125783e-17
+// 很明显 0.1 + 0.2 不等于 0.3, 我们可以通过上面的方法来检测误差
+function withinErrorMargin (left, right) {
+	return Math.abs(left - right) < Number.EPSILON * Math.pow(2, 2);
+}
+
+0.1 + 0.2 === 0.3 // false
+withinErrorMargin(0.1 + 0.2, 0.3); //true
+1.1 + 1.3 === 2.4 // false
+withinErrorMargin(1.1 + 1.3, 2.4) // true
+
 
 //es6 变量范围Number.MAX_SAFE_INTEGER和Number.MIN_SAFE_INTEGER这两个常量，用来表示这个范围的上下限
 console.log(Number.MAX_SAFE_INTEGER);
